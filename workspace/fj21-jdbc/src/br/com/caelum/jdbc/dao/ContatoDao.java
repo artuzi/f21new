440929016b523595c5745c16d22aa1acdb8ad772
Denis Artuzi <artuzi@gmail.com>
2017-03-21 22:33:17 -0300

    Inclui remove e altera

--- workspace/fj21-jdbc/src/br/com/caelum/jdbc/dao/ContatoDao.java
+++ workspace/fj21-jdbc/src/br/com/caelum/jdbc/dao/ContatoDao.java
@@ -68,4 +68,66 @@ public class ContatoDao {
 			throw new RuntimeException(e);
 		}
 	}
+	
+	public Contato pesquisa(int id){
+
+		String sql = "select * from contatos where id = ?";
+
+		Contato contato = new Contato();
+
+		
+		try {
+			PreparedStatement stmt = this.connection.prepareStatement(sql);
+			stmt.setLong(1, id);
+			ResultSet rs = stmt.executeQuery();
+			
+			rs.next();
+			
+			contato.setId(rs.getLong("id"));
+			contato.setNome(rs.getString("nome"));
+			contato.setEmail(rs.getString("email"));
+			contato.setEndereco(rs.getString("endereco"));
+			
+			Calendar data = Calendar.getInstance();
+			data.setTime(rs.getDate("dataNascimento"));
+			contato.setDataNascimento(data);
+
+			rs.close();
+			stmt.close();
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}		
+		return contato;
+
+	}
+	
+	public void altera(Contato contato) {
+		String sql = "update contatos set nome=?, email=?, endereco=?, dataNascimento=? where id=?";
+		
+		try {
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setString(1, contato.getNome());
+			stmt.setString(2, contato.getEmail());
+			stmt.setString(3, contato.getEndereco());
+			stmt.setDate(4, new Date(contato.getDataNascimento().getTimeInMillis()));
+			stmt.setLong(5, contato.getId());
+			stmt.execute();
+			stmt.close();
+			
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void remove(Contato contato){
+		try {
+			PreparedStatement stmt = connection.prepareStatement("delete from contatos where id=?");
+			stmt.setLong(1, contato.getId());
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
