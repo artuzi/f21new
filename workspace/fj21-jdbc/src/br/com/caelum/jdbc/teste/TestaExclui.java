14c15dc078b11057bd4cb042ce86c1067ff79be1
Denis Artuzi <artuzi@gmail.com>
2017-03-21 22:40:21 -0300

    Teste de exclusao

---
+++ workspace/fj21-jdbc/src/br/com/caelum/jdbc/teste/TestaExclui.java
@@ -0,0 +1,19 @@
+package br.com.caelum.jdbc.teste;
+
+import br.com.caelum.jdbc.dao.ContatoDao;
+import br.com.caelum.jdbc.modelo.Contato;
+
+public class TestaExclui {
+	public static void main(String[] args) {
+		ContatoDao dao = new ContatoDao();
+		
+		Contato contato = new Contato();
+		
+		contato.setId(4L);
+		
+		dao.remove(contato);
+		
+		System.out.println("Registro excluido: " + contato.getId());
+		
+	}
+}
