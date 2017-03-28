94dcf35755a1648c2a6f53303aaf6d48f444e3be
Denis Artuzi <artuzi@gmail.com>
2017-03-28 19:45:01 -0300

    Aula 07 Exercicio 9.7

--- workspace/fj21-agenda/src/br/com/caelum/testes/Gerajson.java
+++ workspace/fj21-agenda/src/br/com/caelum/testes/Gerajson.java
@@ -2,7 +2,13 @@ package br.com.caelum.testes;
 
 import com.google.gson.Gson;
 
+import br.com.caelum.agenda.modelo.Contato;
+
 public class Gerajson {
 
+	public Gerajson(Contato contato){
 	Gson gson = new Gson();
+	
+	System.out.println(gson.toJson(contato));	
+	}
 }
