f10d947b9b3be5f6397c93f80e8fb840eaf2ce4d
Denis Artuzi <artuzi@gmail.com>
2017-03-28 22:19:05 -0300

    Aula 07 - Exercicio 10.6

--- workspace/fj21-agenda/src/br/com/caelum/mvc/logica/ListaContatosLogic.java
+++ workspace/fj21-agenda/src/br/com/caelum/mvc/logica/ListaContatosLogic.java
@@ -1,5 +1,6 @@
 package br.com.caelum.mvc.logica;
 
+import java.sql.Connection;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,7 +13,9 @@ public class ListaContatosLogic implements Logica {
 
 	public String executa(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		
-		List<Contato> contatos = new ContatoDao().getLista();
+		Connection connection = (Connection) req.getAttribute("conexao");
+		
+		List<Contato> contatos = new ContatoDao(connection).getLista();
 		
 		req.setAttribute("contatos", contatos);
 		
